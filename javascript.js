0428dd8feba14e7a770c91f308ec7bd453e8af46
DavideVecchio01 <95300391+DavideVecchio01@users.noreply.github.com>
2023-06-24 11:55:08 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
+document.getElementById("cookie-accept").addEventListener("click", function() {
+  // Qui puoi inserire il tuo codice per gestire l'accettazione dei cookie
+  // Ad esempio, puoi nascondere il banner dei cookie o impostare una variabile di stato per tener traccia dell'accettazione
+  document.getElementById("cookie-banner").style.display = "none";
+});
 
