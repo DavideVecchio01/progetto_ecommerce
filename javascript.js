da798d19af91d95b24a958b995071da4071d0501
DavideVecchio01 <95300391+DavideVecchio01@users.noreply.github.com>
2023-06-24 11:31:13 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,1 @@
-function scrollToTop() {
-           window.scrollTo(0, 0);
-        }
-function showCookie() {
-      let cookie = document.querySelector('#cookie');
-      cookie.style = "display:block;";
-    }
-//cookie
-function hideCookie() {
-      let cookie = document.querySelector('#cookie');
-      cookie.style = "display:none;";
-    }
+
