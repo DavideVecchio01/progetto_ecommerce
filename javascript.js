6536c1ae36d0ef4d6dedf52d39cae47d3a4dd290
DavideVecchio01 <95300391+DavideVecchio01@users.noreply.github.com>
2023-06-24 11:47:35 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,4 @@
-
+document.getElementById("cookie-icon").addEventListener("click", function() {
+  // Qui puoi inserire il tuo codice per gestire il clic sull'icona dei cookie
+  console.log("Hai cliccato sull'icona dei cookie!");
+});
